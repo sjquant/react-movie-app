c669728562a089c3e8277e0f4e8380d9ef94ec95
sjquant <seonujang92@gmail.com>
2020-09-26 21:53:50 +0900

    Implement infinite scroll for movies

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,32 @@
 import React from "react";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 import Movie from "../components/Movie";
 import "./Home.css";
 
 class Home extends React.Component {
   state = {
-    isLoading: true,
+    isLoading: false,
     movies: [],
+    page: 1,
   };
   getMovies = async () => {
-    const {
+    const { page } = this.state;
+    this.setState({ isLoading: true });
+    let {
       data: {
         data: { movies },
       },
     } = await axios.get("https://yts-proxy.now.sh/list_movies.json", {
       params: {
         sort_by: "rating",
+        page: page,
       },
     });
-    this.setState({ movies, isLoading: false });
+    this.setState((state) => {
+      movies = [...state.movies, ...movies];
+      return { movies, page: state.page + 1, isLoading: false };
+    });
   };
   componentDidMount() {
     this.getMovies();
@@ -27,25 +35,29 @@ class Home extends React.Component {
     const { isLoading, movies } = this.state;
     return (
       <section className="container">
-        {isLoading ? (
+        {isLoading && (
           <div className="loader">
             <span className="loader__text">Loading...</span>
           </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
         )}
+        <InfiniteScroll
+          className="movies"
+          dataLength={movies.length}
+          next={this.getMovies}
+          hasMore={true}
+        >
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          ))}
+        </InfiniteScroll>
       </section>
     );
   }
