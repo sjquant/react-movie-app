635dab6874d208c0aa061f59d99690653166f58b
sjquant <seonujang92@gmail.com>
2020-09-26 22:01:46 +0900

    Updates

---
+++ precache-manifest.9d2a75adab3d5725e800c5af60b9e13c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cc3aa362d835dec403bc6dab218858",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "f6d1752a2cf49897d81d",
+    "url": "/react-movie-app/static/css/main.9f132a9b.chunk.css"
+  },
+  {
+    "revision": "29a2830a284e30241fb1",
+    "url": "/react-movie-app/static/js/2.064e3629.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie-app/static/js/2.064e3629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d1752a2cf49897d81d",
+    "url": "/react-movie-app/static/js/main.cb01f105.chunk.js"
+  },
+  {
+    "revision": "651af530d224a688009d",
+    "url": "/react-movie-app/static/js/runtime-main.c0625075.js"
+  }
+]);
